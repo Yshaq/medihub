43d668a76a933765e1c0cb58452e70502ed93309
anks321 <kumaralankrit20@gmail.com>
2021-11-03 23:14:28 +0530

    views

--- hospital/views.py
+++ hospital/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,Group
+from .models import *
+from django.contrib.auth import authenticate,logout,login
+from django.utils import timezone
 
 # Create your views here.
 def landingPageView(request):
@@ -6,3 +11,19 @@ def landingPageView(request):
 
 def indexView(request):
     return render(request, 'hospital/index.html')
+def Login_admin(request):
+	error = ""
+	if request.method == 'POST':
+		u = request.POST['username']
+		p = request.POST['password']
+		user = authenticate(username=u,password=p)
+		try:
+			if user.is_staff:
+				login(request,user)
+				error = "no"
+			else:
+				error = "yes"
+		except:
+			error = "yes"
+	d = {'error' : error}
+	return render(request,'login.html',d)
